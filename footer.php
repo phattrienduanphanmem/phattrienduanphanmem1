968f28c349dfc3e6eaef966620d32382ed18ff76
thangdt72 <57880551+thangdt72@users.noreply.github.com>
2020-07-08 22:45:39 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,63 @@
+<div class="container">
+  <div id="mainContent-inner">
+  </div>
+</div>
+<footer class="footer-page">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 text-left">
+            <h3><span id="dnn_dnnCopyright_lblCopyright" class="SkinObject">©  2020 - TRƯỜNG ĐẠI HỌC THỦY LỢI</span></h3>
+          </div>
+          <div class="col-md-4 text-right">
+            <a href="https://www.facebook.com/daihocthuyloi1959/?fref=ts" target="_blank"><img src="skins/images/fb-icon.png"></a>
+            <a href="https://www.youtube.com/user/daihocthuyloi" target="_blank"><img src="skins/images/ytb-icon.png"></a>
+            <a href="https://twitter.com/Daihocthuyloihn" target="_blank"><img src="skins/images/twitter-icon.png"></a>
+          </div>
+        </div>
+      </div>
+    </div><!-- /.footer-top -->
+    <div class="footer-middle">
+      <div class="container">
+	<div class="row">
+<div class="col-md-4"><a href="#" target="_blank"><img alt="Image" src="skins/images/TLU-map.png"> </a></div>
+<div class="col-md-8">
+<div class="row">
+<div class="col-md-6">
+<div class="address">TRƯỜNG ĐẠI HỌC THỦY LỢI<br>
+Địa chỉ : 175 TÂY SƠN, ĐỐNG ĐA, HÀ NỘI.<br>
+Điện thoại: (024) 3852 2201 - Fax: (024) 3563 3351<br>
+Email: <a href="#">phonghcth@tlu.edu.vn</a></div>
+
+<hr>
+<div class="address">PHÂN HIỆU ĐẠI HỌC THỦY LỢI<br>
+Địa chỉ : Số 2 - Đường Trường Sa - P.17 - Q.Bình Thạnh - Tp.Hồ Chí Minh<br>
+Điện thoại: (84)028.38&nbsp;400 532 - Fax: (84)028.38 400542<br>
+Email: <a href="#">cs2@tlu.edu.vn</a></div>
+</div>
+
+<div class="col-md-6">
+<div class="address">PHÂN HIỆU ĐẠI HỌC THỦY LỢI<br>
+Phường An Thạnh - TX Thuận An - Tỉnh Bình Dương<br>
+Điện thoại: (84).650.3748 620<br>
+Fax:(84).650.3833 489</div>
+
+<hr>
+<div class="address">VIỆN ĐÀO TẠO VÀ KHOA HỌC ỨNG DỤNG MIỀN TRUNG<br>
+Địa chỉ: Số 115 Trần Phú, Thành phố&nbsp;Phan Rang, Tỉnh Ninh Thuận<br>
+Điện thoại: 02593.823 027, 02593.823 028<br>
+Email: <a href="#"> trungtamdh2@tlu.edu.vn</a></div>
+</div>
+</div>
+</div>
+</div>
+
+      </div>
+    </div><!-- /.footer-middle -->
+  </footer>
+    <script src="skins/js/jquery-3.4.1.min.js"></script>
+    <script src="skins/js/bootstrap.min.js" ></script>
+    <script src="skins/js/javascrip.js" ></script>
+    </form>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Quản Lý Lịch Trình Giảng Dạy</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="skins/css/bootstrap.min.css" >
+    <link rel="stylesheet" href="skins/css/all.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="xet/style.css">
+  </head>
+  <body>
+  <form method="post" action="/" id="Form" enctype="multipart/form-data">
+<!-- BEGIN: HEADER -->
+<header class="header-page">
+    <div class="container">
+      <div class="row align-items-end">
+        <div class="col-md-7">
+          <div class="header-brand">
+            <a href="/">
+                <img src="skins/images/logo.png" alt="Trường Đại học Thủy lợi" class="d-none d-md-block">
+            </a>
+          </div>
+        </div>
+        <div class="col-md-5">
+          <div class="header-right">
+            <ul class="header-links">
+              <li><a href="#">Đăng ký</a></li>
+              <li><a href="#">Đăng nhập</a></li>
+              <li><a href="#">Liên hệ</a></li>
+            </ul>
+<!--Begin Search-->
+    <div class="header-search ng-scope" ng-controller="myCntrl1533">
+        <div class="form-search">
+            <input class="form-control" type="text" id="tbxHomekeyword" placeholder="Tìm kiếm">
+            <input type="button" class="form-submit" ng-click="formClick()">
+        </div>
+    </div>  
+<!--End Search-->
+            <div class="languages">
+              <a href="#"></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+<!-- END: HEADER -->
+<section class="section-banner">
+    <div class="navigation">
+      <div class="container">
+    <nav class="mainmenu" id="mainmenu">
+    <ul class="menu clearfix">
+            <li><a href="index.php"><i class="fa fa-home" aria-hidden="true" style="font-size:130%"></i></a></li>
+            <li><a href="http://www.tlu.edu.vn">TLU</a></li>
+            <li>
+                <a href="tuyensinhdaihoc.php">Tuyển sinh đại học</a>
+            </li>
+            <li>
+                <a href="tintuyensinhthacsi.php">Tuyển sinh thạc sĩ</a>
+            </li>
+            <li>
+                <a href="tuyensinhtiensi.php">Tuyển sinh tiến sĩ</a>
+            </li>
+            <li>
+                <a>Ngành đào tạo</a>
+                <ul class="sub-menu">
+                    <li><a  href="nganhdaotaodaihoc.php">Ngành đào tạo ĐẠI HỌC</a></li>
+                    <li><a  href="nganhdaotaothacsi.php">Ngành đào tạo THẠC SĨ</a></li>
+                    <li><a  href="nganhdaotaotiensi.php">Ngành Đào tạo TIẾN SĨ</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="xet/xettuyentructuyen.php">Đăng ký xét tuyển đại học</a>
+            </li>
+        </ul>
+        </nav>
+    </div>
+</div>
+</section>
+<div style="margin-top:50px"></div>
